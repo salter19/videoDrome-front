73dccc07c170dc7f83ac3bcd9123836cf6ad37b9
salter19 <salonenterhi19@gmail.com>
2021-01-03 15:41:13 +0200

    Add to SearchOMDB: useEffect

--- src/components/SearchOMDB.js
+++ src/components/SearchOMDB.js
@@ -1,7 +1,42 @@
 import React, { useState, useEffect} from 'react';
 
 const SearchOMDB = () => {
-  
+
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+
+  useEffect(()=> {
+    
+  }, [title, year]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <div className="search title">
+            <label>Enter Movie Title:</label>
+            <input 
+              className="input" 
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="search year">
+            <label>Enter Release Year:</label>
+            <input 
+              className="input" 
+              value={year}
+              onChange={e => setYear(e.target.value)}
+            />
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+  );
+
 };
 
 export default SearchOMDB;
