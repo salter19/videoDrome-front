1f0678a5496b68960afb448911df0c451858ad90
salter19 <salonenterhi19@gmail.com>
2021-01-03 18:03:22 +0200

    Add to AddMovieForm: skeleton.

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 const AddForm = () => {
-  
+
+  const [title, setTitle] = useState('');
+  const [country, setCountry] = useState([]);
+  const [year, setYear] = useState('');
+
+
   return (
     <div>add form</div>
   );
