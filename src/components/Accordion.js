8a049e5fac994047bfebdc55a9fb1aece739c89c
salter19 <salonenterhi19@gmail.com>
2021-01-03 14:46:11 +0200

    Add to Accordion: functionality to open and close options.

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -9,27 +9,31 @@ const Accordion = ( {items} ) => {
   };
 
   const renderedItems = items.map((item, index) => {
+
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={item.title}>
         <div 
-          className="title active"
+          className={`title ${active}`}
           onClick={() => onTitleClick(index)}
         >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
 
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.year}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return <div className="ui styled accordion">
+  return (
+    <div className="ui styled accordion">
       {renderedItems}
-      <h1>{activeIndex}</h1>
     </div>
+  );
 };
 
 export default Accordion;
