779b74bafb1860f191744d81f892c37d9b42ef82
salter19 <salonenterhi19@gmail.com>
2021-01-04 13:13:59 +0200

    Refactor SearchOMDB: do search only by button click.

--- src/components/SearchOMDB.js
+++ src/components/SearchOMDB.js
@@ -1,7 +1,7 @@
 import './../style/SearchOMDB.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import AddForm from './AddMovieForm'
+// import AddForm from './AddMovieForm'
 
 const _url = 'http://localhost:8080/movieDB/omdb/' 
 
@@ -13,6 +13,7 @@ const SearchOMDB = () => {
 
   console.log(results);
 
+  /*
   useEffect(() => {
     const func = async () => {
       const { data } = await axios.get(`${_url}${title}/${year}`);
@@ -26,18 +27,34 @@ const SearchOMDB = () => {
     }
 
   }, [title, year]);
+  */
+
+  const onButtonClick = () => {
+    (async() => {
+
+      if (title && year) {
+        const { data } = await axios.get(`${_url}${title}/${year}`);
+        setResults(data);
+      } else if (title) {
+        const { data } = await axios.get(`${_url}${title}`);
+        setResults(data);        
+      }
+    })();
+  }
 
   return (
     <div className="omdb">
-      <div className="ui segment">
-        <h4 className="ui dividing header">Add Movie To Database</h4>
+      <div className="omdb ui segment">
+        <h4 className="omdb ui dividing header">Add Movie To Database</h4>
         <div className="ui form">
           <div className="two fields">
 
             <div className="nine wide field">
-              <label>Movie Title:</label>
+              <label className="title label">Movie Title:</label>
               <input 
                 className="input" 
+                type="text"
+                placeholder="Return of Jedi"
                 value={title}
                 onChange={e => setTitle(e.target.value)}
               />
@@ -46,15 +63,27 @@ const SearchOMDB = () => {
             <div className="one wide field"></div>
 
             <div className="four wide field">
-              <label>Release Year:</label>
+              <label className="year label">Release Year:</label>
               <input 
-                className="input" 
+                className="input"
+                type="text"
+                placeholder="1983"
                 value={year}
                 onChange={e => setYear(e.target.value)}
               />
             </div>
 
           </div>
+          
+          <div className="button center">
+            <button 
+              className="ui button"
+              onClick={onButtonClick}
+            >
+              Videofinder General
+            </button>
+          </div>
+
         </div>
       </div>
     </div>
