8595b4fe2e0362ada7a5549025d3fd4330ece11b
salter19 <salonenterhi19@gmail.com>
2021-01-05 13:33:36 +0200

    Add to AddMovieForm: functionality to onInput - onSaveClick.

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,21 +1,66 @@
 import './../style/AddMovieForm.css'
 import React, { useState } from 'react';
 import OneField from './subcomponents/OneField';
-//import axios from 'axios';
+import axios from 'axios';
 
 const AddForm = ({ movie }) => {
 
   const placeholder = ' - ';
   const full = 'sixteen';
   const half = 'eight';
+  const titles = {
+    name: 'Title',
+    year: 'Release year',
+    genres: 'Genre',
+    category: 'Category',
+    director: 'Director',
+    country: 'Country',
+    sub: 'Subtitles', 
+    format: 'Format',
+    actors: 'Actors'
+  }
+  const [addition, setAddition] = useState({
+    name: movie.name,
+    category: movie.category,
+    genres: movie.genres,
+    year: movie.year,
+    director: movie.director,
+    country: movie.country,
+    sub : movie.sub,
+    sub2: movie.sub2,
+    format: movie.format,
+    format2: movie.format,
+    actors: movie.actors,
+    imdbID: movie.imdbID
+  });
 
-
-  const onSaveClick = () => {
+  const onSaveClick = async() => {
     console.log('saving...');
+    console.log(addition);
+    const post = await axios.post(`http://localhost:8080/movieDB/`, addition);
+    post ? console.log('done!') : console.log('...');
   }
 
   const onInput = (value, title) => {
-    console.log(title + ' onInput: ' + value);
+    const keysA = Object.keys(addition);
+    const keysT = Object.keys(titles);
+    const tmp = addition;
+    let titleKey;
+
+    for (let key of keysT) {
+      if (titles[key] === title) {
+        titleKey = key;
+      }        
+    }
+
+    if (titleKey) {
+      for (let key of keysA) {
+        if (key === titleKey) {
+          tmp[key] = value;
+        }        
+      }
+    }
+    setAddition(tmp);
   }
   
   return (
@@ -28,31 +73,31 @@ const AddForm = ({ movie }) => {
             <div className="ui internally celled grid">
 
               <div className="row">
-                {OneField(full, 'Title', placeholder, movie.name, onInput)}
+                {OneField(full, titles.name, placeholder, movie.name, onInput)}
               </div>
 
               <div className="row">   
-                {OneField(half, 'Genre', placeholder, movie.genres)} 
-                {OneField(half, 'Release Year', placeholder, movie.year)}             
+                {OneField(half, titles.genres, placeholder, movie.genres, onInput)} 
+                {OneField(half, titles.year, placeholder, movie.year, onInput)}             
               </div>
 
               <div className="row">
-                {OneField(full, 'Director', placeholder, movie.director)}
+                {OneField(half, titles.category, placeholder, movie.category, onInput)}
+                {OneField(half, titles.director, placeholder, movie.director, onInput)}
               </div>
 
               <div className="row">
-                {OneField(half, 'Actor 1', placeholder, movie.actors[0])}
-                {OneField(half, 'Actor 2', placeholder, movie.actors[1])}
+                {OneField(full, titles.actors, placeholder, movie.actors, onInput)}
               </div>
 
               <div className="row">  
-                {OneField(half, 'Format', 'dvd')}  
-                {OneField(half, 'Subtitles', placeholder)}             
+                {OneField(half, titles.format, 'dvd', placeholder, onInput)}  
+                {OneField(half, titles.sub, placeholder, placeholder, onInput)}             
               </div>
 
               <div className="row">   
-                {OneField(half, 'Country', placeholder, movie.country)} 
-                {OneField(half, 'Classification', placeholder)}             
+                {OneField(half, titles.country, placeholder, movie.country, onInput)} 
+                {OneField(half, 'Classification', placeholder, placeholder, onInput)}             
               </div>
 
             </div>
